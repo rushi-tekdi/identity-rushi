4ee7a6d99a9bf8b1be576a10797d051f254ec790
Rushi Gawali <rushi_g@tekditechnologies.com>
2023-09-01 15:22:14 +0530

    path added

--- src/did/vault.service.ts
+++ src/did/vault.service.ts
@@ -5,6 +5,7 @@ const Vault = require('hashi-vault-js');
 @Injectable()
 export class VaultService {
   private token = process.env.VAULT_TOKEN;
+  private vaultpath = process.env.VAULT_PATH;
   private vault = new Vault({
     https: false,
     baseUrl: `${process.env.VAULT_ADDR}/v1`,
@@ -15,6 +16,7 @@ export class VaultService {
   async checkStatus() {
     const status = await this.vault.healthCheck();
     console.log("checkStatus this.token",this.token);
+    console.log("checkStatus this.vaultpath",this.vaultpath);
     console.log("checkStatus this.vault",JSON.stringify(this.vault));
     await this.vault.readKVEngineConfig(this.token);
     console.log("checkStatus status",JSON.stringify(status));
@@ -22,13 +24,16 @@ export class VaultService {
   }
   async writePvtKey(secret: string, name: string, path?: string) {
     console.log("writePvtKey this.token",this.token);
+    console.log("writePvtKey this.vaultpath",this.vaultpath);
     console.log("writePvtKey this.vault",JSON.stringify(this.vault));
     console.log("writePvtKey secret",secret);
     console.log("writePvtKey name",name);
     console.log("writePvtKey path",path);
+
     const createSecret = await this.vault.createKVSecret(
       this.token,
-      path ? path + `/${name}` : `ulp/identity-ms/private_keys/${name}`,
+      //path ? path + `/${name}` : `ulp/identity-ms/private_keys/${name}`,
+      this.vaultpath,
       secret,
     );
     console.log("writePvtKey createSecret",JSON.stringify(createSecret));
@@ -37,12 +42,14 @@ export class VaultService {
 
   async readPvtKey(name: string, path?: string) {
     console.log("readPvtKey this.token",this.token);
+    console.log("readPvtKey this.vaultpath",this.vaultpath);
     console.log("readPvtKey this.vault",JSON.stringify(this.vault));
     console.log("readPvtKey name",name);
     console.log("readPvtKey path",path);
     const read = await this.vault.readKVSecret(
       this.token,
-      path ? path + `/${name}` : `ulp/identity-ms/private_keys/${name}`,
+      //path ? path + `/${name}` : `ulp/identity-ms/private_keys/${name}`,
+      this.vaultpath
     );
     console.log("readPvtKey read",JSON.stringify(read));
     return read.data;
